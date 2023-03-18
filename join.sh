9cddf9e32a2473f9b5705f1eb0d5a9de561f303e
Sebastian Nowak <nowak-ninja@users.noreply.github.com>
2023-03-18 11:16:52 +0100

    Update join.sh

--- join.sh
+++ join.sh
@@ -2,6 +2,11 @@
 
 IP=$1
 
+if [[ ! -n "$IP" ]]; then
+  echo "No IP provided, use: ${0} 1.2.3.4"
+  exit 1
+fi
+
 docker run -d \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v /var/lib/docker/volumes:/var/lib/docker/volumes \
